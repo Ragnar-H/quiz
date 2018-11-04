4cf51fe55af2cd14a3486e36be56ca05a17b8103
Ragnar-H <ragnarhardarson90@gmail.com>
2018-11-04 16:53:54 +0000

    Add story for Question

---
+++ src/Question.stories.tsx
@@ -0,0 +1,13 @@
+import * as React from "react";
+import { storiesOf } from "@storybook/react";
+import { action } from "@storybook/addon-actions";
+import { Question } from "./Question";
+
+storiesOf("Components/Question", module).add("default", () => (
+  <Question
+    questionId="some-id"
+    questionText="Who is the president of Romania?"
+    answer="James Bond"
+    onQuestionClick={action("onQuestionClick")}
+  />
+));
