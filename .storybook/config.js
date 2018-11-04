392a42082591b294411678c2589dacacf35be683
Ragnar-H <ragnarhardarson90@gmail.com>
2018-11-04 16:12:18 +0000

    Add app wide css to storybook

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,5 +1,6 @@
 import { configure } from "@storybook/react";
 
+import "../src/index.css";
 const req = require.context("../src", true, /.stories.tsx$/);
 function loadStories() {
   req.keys().forEach(filename => req(filename));
