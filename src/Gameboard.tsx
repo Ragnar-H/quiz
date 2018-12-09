e06f0224586aa32ead94bcf0515a66a18a7ed6c1
Ragnar-H <ragnarhardarson90@gmail.com>
2018-12-09 16:20:38 +0100

    Convert Gameboard to function component

--- src/Gameboard.tsx
+++ src/Gameboard.tsx
@@ -12,37 +12,35 @@ interface Props {
 
 interface State {}
 
-export class Gameboard extends React.Component<Props, State> {
-  render() {
-    const { categories, questions, handleSetCurrentQuestion } = this.props;
-    return (
-      <div className={styles.gameboard}>
-        {categories.map((category, categoryIndex) => (
-          <React.Fragment key={category.id}>
-            <Cell row={1} column={categoryIndex}>
-              {category.name}
-            </Cell>
-            {questions
-              .filter(question => question.category.id === category.id)
-              .sort((a, b) => a.points - b.points)
-              .map((question, questionIndex) => (
-                <Cell
-                  key={question.id}
-                  row={questionIndex + 2}
-                  column={categoryIndex}
-                >
-                  <Question
-                    questionId={question.id}
-                    questionText={question.text}
-                    answer={question.answer}
-                    onQuestionClick={handleSetCurrentQuestion}
-                    points={question.points}
-                  />
-                </Cell>
-              ))}
-          </React.Fragment>
-        ))}
-      </div>
-    );
-  }
+export function Gameboard(props: Props) {
+  const { categories, questions, handleSetCurrentQuestion } = props;
+  return (
+    <div className={styles.gameboard}>
+      {categories.map((category, categoryIndex) => (
+        <React.Fragment key={category.id}>
+          <Cell row={1} column={categoryIndex}>
+            {category.name}
+          </Cell>
+          {questions
+            .filter(question => question.category.id === category.id)
+            .sort((a, b) => a.points - b.points)
+            .map((question, questionIndex) => (
+              <Cell
+                key={question.id}
+                row={questionIndex + 2}
+                column={categoryIndex}
+              >
+                <Question
+                  questionId={question.id}
+                  questionText={question.text}
+                  answer={question.answer}
+                  onQuestionClick={handleSetCurrentQuestion}
+                  points={question.points}
+                />
+              </Cell>
+            ))}
+        </React.Fragment>
+      ))}
+    </div>
+  );
 }
