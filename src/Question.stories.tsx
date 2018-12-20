27ef5ea9fec0588c62c59fc5b63e8e8610d58388
Ragnar-H <ragnarhardarson90@gmail.com>
2018-12-20 09:22:16 +0100

    Add width wrapper for Question
    Makes is slightly easier to work with

--- src/Question.stories.tsx
+++ src/Question.stories.tsx
@@ -3,44 +3,59 @@ import { storiesOf } from "@storybook/react";
 import { action } from "@storybook/addon-actions";
 import { Question } from "./Question";
 
+interface QuestionContainerProps {
+  children: any;
+}
+function QuestionContainer(props: QuestionContainerProps, children: any) {
+  return <div style={{ maxWidth: "300px" }}>{props.children}</div>;
+}
+
 storiesOf("Components/Question", module)
   .add("editing", () => (
-    <Question
-      mode="editing"
-      questionId="some-id"
-      questionText="Who is the president of Romania?"
-      answer="James Bond"
-      onQuestionClick={action("onQuestionClick")}
-      points={500}
-    />
+    <QuestionContainer>
+      <Question
+        mode="editing"
+        questionId="some-id"
+        questionText="Who is the president of Romania?"
+        answer="James Bond"
+        onQuestionClick={action("onQuestionClick")}
+        points={500}
+      />
+    </QuestionContainer>
   ))
   .add("answered", () => (
-    <Question
-      mode="answered"
-      questionId="some-id"
-      questionText="Who is the president of Romania?"
-      answer="James Bond"
-      onQuestionClick={action("onQuestionClick")}
-      points={500}
-    />
+    <QuestionContainer>
+      <Question
+        mode="answered"
+        questionId="some-id"
+        questionText="Who is the president of Romania?"
+        answer="James Bond"
+        onQuestionClick={action("onQuestionClick")}
+        points={500}
+      />
+    </QuestionContainer>
   ))
   .add("answering", () => (
-    <Question
-      mode="answering"
-      questionId="some-id"
-      questionText="Who is the president of Romania?"
-      answer="James Bond"
-      onQuestionClick={action("onQuestionClick")}
-      points={500}
-    />
+    <QuestionContainer>
+      <Question
+        mode="answering"
+        questionId="some-id"
+        questionText="Who is the president of Romania?"
+        answer="James Bond"
+        onQuestionClick={action("onQuestionClick")}
+        points={500}
+      />
+    </QuestionContainer>
   ))
   .add("unanswered", () => (
-    <Question
-      mode="unanswered"
-      questionId="some-id"
-      questionText="Who is the president of Romania?"
-      answer="James Bond"
-      onQuestionClick={action("onQuestionClick")}
-      points={500}
-    />
+    <QuestionContainer>
+      <Question
+        mode="unanswered"
+        questionId="some-id"
+        questionText="Who is the president of Romania?"
+        answer="James Bond"
+        onQuestionClick={action("onQuestionClick")}
+        points={500}
+      />
+    </QuestionContainer>
   ));
