c207850effe780bc43fb87a0fd69b8a6b3f6d55b
Ragnar-H <ragnarhardarson90@gmail.com>
2018-12-21 13:24:45 +0000

    Add Toggle to Gameboard for switching between modes

--- src/Gameboard.tsx
+++ src/Gameboard.tsx
@@ -1,8 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 import { Cell } from "./Cell";
 import styles from "./Gameboard.module.css";
 import { Question } from "./Question";
 import { Category } from "./Category";
+import { ToggleLabel, Toggle } from "./Toggle";
 
 interface Props {
   categories: Array<ICategory>;
@@ -17,47 +18,63 @@ interface Props {
 export function Gameboard(props: Props) {
   const {
     categories,
-    editMode,
     onSetCurrentQuestion,
     onSubmitCategoryEdit,
     onSubmitQuestionEdit,
-    questions
+    questions,
+    editMode: initialEditMode
   } = props;
-  return (
-    <div className={styles.gameboard}>
-      {categories.map((category, categoryIndex) => (
-        <React.Fragment key={category.id}>
-          <Cell row={1} column={categoryIndex + 1}>
-            <Category
-              categoryId={category.id}
-              categoryName={category.name}
-              mode={editMode ? "editing" : "answering"}
-              onCategoryEdit={onSubmitCategoryEdit}
-            />
-          </Cell>
 
-          {questions
-            .filter(question => question.category === category.id)
-            .sort((a, b) => a.points - b.points)
-            .map((question, questionIndex) => (
-              <Cell
-                key={question.id}
-                row={questionIndex + 2}
-                column={categoryIndex + 1}
-              >
-                <Question
-                  questionId={question.id}
-                  questionText={question.text}
-                  answer={question.answer}
-                  onQuestionClick={onSetCurrentQuestion}
-                  onQuestionEdit={onSubmitQuestionEdit}
-                  points={question.points}
-                  mode={editMode ? "editing" : "unanswered"}
-                />
-              </Cell>
-            ))}
-        </React.Fragment>
-      ))}
+  const [editMode, setEditMode] = useState(initialEditMode);
+
+  const updateToggle = (label: ToggleLabel) => setEditMode(label.value);
+  const editLabels = [
+    { label: "Preview mode", value: false },
+    { label: "Show edit mode", value: true }
+  ];
+
+  return (
+    <div>
+      <Toggle
+        labels={editLabels}
+        initialLabel={initialEditMode ? editLabels[1] : editLabels[0]}
+        onChange={updateToggle}
+      />
+      <div className={styles.gameboard}>
+        {categories.map((category, categoryIndex) => (
+          <React.Fragment key={category.id}>
+            <Cell row={1} column={categoryIndex + 1}>
+              <Category
+                categoryId={category.id}
+                categoryName={category.name}
+                mode={editMode ? "editing" : "answering"}
+                onCategoryEdit={onSubmitCategoryEdit}
+              />
+            </Cell>
+
+            {questions
+              .filter(question => question.category === category.id)
+              .sort((a, b) => a.points - b.points)
+              .map((question, questionIndex) => (
+                <Cell
+                  key={question.id}
+                  row={questionIndex + 2}
+                  column={categoryIndex + 1}
+                >
+                  <Question
+                    questionId={question.id}
+                    questionText={question.text}
+                    answer={question.answer}
+                    onQuestionClick={onSetCurrentQuestion}
+                    onQuestionEdit={onSubmitQuestionEdit}
+                    points={question.points}
+                    mode={editMode ? "editing" : "unanswered"}
+                  />
+                </Cell>
+              ))}
+          </React.Fragment>
+        ))}
+      </div>
     </div>
   );
 }
