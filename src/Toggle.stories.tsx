8d596bfd1fdc5dd0af5478717fb62cbf9bfd964d
Ragnar-H <ragnarhardarson90@gmail.com>
2018-12-22 19:30:46 +0000

    Update Toggle stories in accordance with new API

--- src/Toggle.stories.tsx
+++ src/Toggle.stories.tsx
@@ -3,10 +3,11 @@ import { storiesOf } from "@storybook/react";
 import { Toggle } from "./Toggle";
 import { action } from "@storybook/addon-actions";
 
+const labels = [{ label: "foo", value: true }, { label: "bar", value: false }];
 storiesOf("Components/Toggle", module).add("default", () => (
   <Toggle
-    initialLabel="foo"
-    labels={["foo", "bar"]}
+    initialLabel={labels[0]}
+    labels={labels}
     onChange={action("onChange")}
   />
 ));
