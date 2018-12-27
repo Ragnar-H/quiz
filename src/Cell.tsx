7129c3360b41afe5890f22d1ff93691f0113d416
Ragnar-H <ragnarhardarson90@gmail.com>
2018-12-27 00:18:48 +0000

    Add static Fullscreen styles to Cell
    For animate-css-grid to work

--- src/Cell.tsx
+++ src/Cell.tsx
@@ -7,21 +7,36 @@ interface Props {
   row?: number;
   endColumn?: number;
   endRow?: number;
+  zIndex?: number;
+  fullscreen?: boolean;
   onClick?: any;
 }
 
 export class Cell extends Component<Props> {
   render() {
-    const { column, row, endColumn, endRow, children } = this.props;
+    const {
+      column,
+      row,
+      endColumn,
+      endRow,
+      zIndex,
+      fullscreen,
+      children
+    } = this.props;
     return (
       <div
-        className={styles.cell}
-        style={{
-          gridColumnStart: column,
-          gridRowStart: row,
-          gridColumnEnd: endColumn,
-          gridRowEnd: endRow
-        }}
+        style={
+          fullscreen
+            ? {}
+            : {
+                gridColumnStart: column,
+                gridRowStart: row,
+                gridColumnEnd: endColumn,
+                gridRowEnd: endRow,
+                zIndex
+              }
+        }
+        className={fullscreen ? styles.cellFullscreen : styles.cell}
       >
         {children}
       </div>
