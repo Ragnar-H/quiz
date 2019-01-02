a4127f946b5bae434e9c1adaaaf827309ae199a7
Ragnar-H <ragnarhardarson90@gmail.com>
2019-01-02 12:32:43 +0000

    Add a default 2:1 aspect ratio to CardText

--- src/CardText.tsx
+++ src/CardText.tsx
@@ -4,15 +4,16 @@ import { Text } from "@vx/text";
 interface Props {
   text: string;
 }
+
 export function CardText(props: Props) {
   return (
-    <svg viewBox="0 0 100 100" height="100%" width="100%">
+    <svg viewBox="0 0 100 50" height="100%" width="100%">
       <Text
         scaleToFit={false}
-        width={100}
-        y={50}
+        width={50}
         height={100}
         x={50}
+        y={25}
         verticalAnchor="middle"
         textAnchor="middle"
       >
